4e4fa4720c791538d8f754d70a34991b5b9d4c98
Lennard Tucholke <lennardtucholke@gmail.com>
2024-04-28 16:11:44 +0200

    added casting

--- generated_tsl/cmake_install.cmake
+++ generated_tsl/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/dertuchi/TSL
+# Install script for directory: /home/dertuchi/work/TSL
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -44,7 +44,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/dertuchi/TSL/generated_tsl/generator_output/build/cmake_install.cmake")
+  include("/home/dertuchi/work/TSL/generated_tsl/generator_output/build/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -55,5 +55,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/dertuchi/TSL/generated_tsl/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/dertuchi/work/TSL/generated_tsl/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
